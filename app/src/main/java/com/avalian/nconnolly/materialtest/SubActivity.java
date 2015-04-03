0e80e9c1f28ed3a9989d9cfb2cede9e4b00a73e9
Neal Connolly <nconnolly@ondeck.com>
2015-04-03 13:57:01 -0400

    finished lesson 4&5

--- app/src/main/java/com/avalian/nconnolly/materialtest/SubActivity.java
+++ app/src/main/java/com/avalian/nconnolly/materialtest/SubActivity.java
@@ -1,29 +1,33 @@
 package com.avalian.nconnolly.materialtest;
 
-import android.content.Intent;
-import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
-    private Toolbar toolbar;
+public class SubActivity extends ActionBarActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setContentView(R.layout.activity_sub);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+
+        // the following lines will me the up/back button appear on this activity
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_sub, menu);
         return true;
     }
 
@@ -38,9 +42,9 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+        if(id==android.R.id.home){
+            NavUtils.navigateUpFromSameTask(this);
 
-        if(id==R.id.navigate){
-            startActivity(new Intent(this, SubActivity.class));
         }
         return super.onOptionsItemSelected(item);
     }
